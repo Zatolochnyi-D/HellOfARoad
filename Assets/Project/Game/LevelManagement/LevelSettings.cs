ff297beed838cb374c691d3fab455567615f2fd8
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2026-09-08 10:49:32 +0300

    Added accessors to Level Settings.

--- Assets/Project/Game/LevelManagement/LevelSettings.cs
+++ Assets/Project/Game/LevelManagement/LevelSettings.cs
@@ -21,5 +21,10 @@ namespace HoaR.Game.LevelManagement
         [SerializeField] private int _segmentsCount; // whole map length is split into this segments, and the middle of animation curve in this segment
                                                      // is used to determine % of max enemy per segment to spawn.
         [SerializeField] private int _randomizationSeed;
+
+        public float LevelLength => _levelLength;
+        public EnemySpawningSetting[] EnemySpawningSettings => _enemySpawningSettings;
+        public int SegmentsCount => _segmentsCount;
+        public int RandomizationSeed => _randomizationSeed;
     }
 }
