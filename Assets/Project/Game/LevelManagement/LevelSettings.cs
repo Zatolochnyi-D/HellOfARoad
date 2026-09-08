57425670da6c834f772722dbf91714f44a4d078a
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2026-09-08 10:44:06 +0300

    Created LevelSettings SO to manage all settings regarding a level.

---
+++ Assets/Project/Game/LevelManagement/LevelSettings.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace HoaR.Game.LevelManagement
+{
+    [System.Serializable]
+    public struct EnemySpawningSetting
+    {
+        public GameObject EnemyPrefab;
+        public AnimationCurve EnemySpawnRatePerSegment;
+        public int MaxEnemyAmountPerSegment;
+    }
+
+    [CreateAssetMenu(fileName = nameof(LevelSettings), menuName = "Project/Level Management/" + nameof(LevelSettings))]
+    public class LevelSettings : ScriptableObject
+    {
+        [Header("Level Length")]
+        [SerializeField] private float _levelLength;
+
+        [Header("Enemy spawning")]
+        [SerializeField] private EnemySpawningSetting[] _enemySpawningSettings;
+        [SerializeField] private int _segmentsCount; // whole map length is split into this segments, and the middle of animation curve in this segment
+                                                     // is used to determine % of max enemy per segment to spawn.
+        [SerializeField] private int _randomizationSeed;
+    }
+}
