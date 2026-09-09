c8f05a222af0c46ef0f4c3ddb4f9232fc31f9b17
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2026-09-09 15:22:23 +0300

    Reconfigured trigger colliders.

--- Assets/Project/Ground/Scripts/GroundTriggerEnterInterceptor.cs
+++ Assets/Project/Ground/Scripts/GroundTriggerEnterInterceptor.cs
@@ -5,12 +5,16 @@ namespace HoaR.Ground
 {
     public class GroundTriggerEnterInterceptor : MonoBehaviour
     {
+        [SerializeField] private LayerMask _triggerOn;
+
         public event Action OnCarEnteredTrigger;
 
         public Transform GroundTransform => transform.parent;
 
         void OnTriggerEnter(Collider other)
         {
+            if (((1 << other.gameObject.layer) & _triggerOn.value) == 0)
+                return;
             OnCarEnteredTrigger?.Invoke();
         }
     }
