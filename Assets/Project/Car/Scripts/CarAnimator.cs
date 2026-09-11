e1382d0c8df41bc7991d71b68e990b8614a836f4
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2026-09-11 11:53:57 +0300

    Made wheel speed controled by wheel size.

--- Assets/Project/Car/Scripts/CarAnimator.cs
+++ Assets/Project/Car/Scripts/CarAnimator.cs
@@ -9,18 +9,21 @@ namespace HoaR.Car
         private static readonly int WHEEL_CLIP_HASH = Animator.StringToHash("WheelRotation");
         private static readonly int SHAKE_CLIP_HASH = Animator.StringToHash("HullShake");
         private static readonly int SHAKE_ANIMATION_DIRECTION_PARAMETER_HASH = Animator.StringToHash("ShakeDirection");
+        private static readonly int WHEEL_SPEED_PARAMETER_HASH = Animator.StringToHash("WheelSpeed");
         private const int WHEEL_LAYER = 0;
         private const int SHAKE_LAYER = 1;
 
         private readonly Animator _animator;
         private readonly Health _health;
+        private readonly CarSettings _settings;
 
         private bool _isReverse = false;
 
-        public CarAnimator(Animator animator, Health health, IGameStateManager<GameState> gameStateManager)
+        public CarAnimator(Animator animator, Health health, IGameStateManager<GameState> gameStateManager, CarSettings settings)
         {
             _animator = animator;
             _health = health;
+            _settings = settings;
 
             gameStateManager.OnStateChanged += HandleGameStateChange;
         }
@@ -37,6 +40,9 @@ namespace HoaR.Car
 
         private void StartAnimation()
         {
+            var wheelSpeed = _settings.MovementSpeed / (2f * Mathf.PI * _settings.WheelRadius);
+            Debug.Log(wheelSpeed);
+            _animator.SetFloat(WHEEL_SPEED_PARAMETER_HASH, wheelSpeed);
             _animator.Play(WHEEL_CLIP_HASH, WHEEL_LAYER, 0f);
 
             _health.OnDamageReceived += _ =>
--- Assets/Project/Car/Scripts/CarSettings.cs
+++ Assets/Project/Car/Scripts/CarSettings.cs
@@ -9,10 +9,12 @@ namespace HoaR.Car
         [SerializeField] private Vector3 _generalMovementVector = Vector3.forward;
         [SerializeField] private float _movementSpeed = 5f;
         [SerializeField] private int _maxHealthPoints = 100;
+        [SerializeField] private float _wheelRadius = 0.7f;
 
         public Vector3 GeneralMovementVector => _generalMovementVector;
         public float MovementSpeed => _movementSpeed;
         public int MaxHealthPoints => _maxHealthPoints;
+        public float WheelRadius => _wheelRadius;
 
     }
 }
