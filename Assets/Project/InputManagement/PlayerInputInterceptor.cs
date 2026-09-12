01ca5782dbecaab5b868350d903e503bda8eb498
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2026-09-12 15:37:22 +0300

    Made PlayerInputInterceptor send OnUp and stop sending OnHorizontalSwipe on game end.

--- Assets/Project/InputManagement/PlayerInputInterceptor.cs
+++ Assets/Project/InputManagement/PlayerInputInterceptor.cs
@@ -1,7 +1,9 @@
 using System;
+using HoaR.Game.GameStateManagement;
 using HoaR.Turret;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using Zenject;
 
 namespace HoaR.InputManagement
 {
@@ -11,19 +13,45 @@ namespace HoaR.InputManagement
         public event Action OnDown;
         public event Action OnUp;
 
+        [Inject] private readonly IGameStateManager<GameState> _gameStateManager;
+
+        private bool _isDown = false;
+
+        void Awake()
+        {
+            _gameStateManager.OnStateChanged += HandleGameStateChange;
+        }
+
+        void OnDestroy()
+        {
+            _gameStateManager.OnStateChanged -= HandleGameStateChange;
+        }
+
+        private void HandleGameStateChange(GameState newState)
+        {
+            if (_isDown && (newState == GameState.GameWon || newState == GameState.GameLost))
+            {
+                OnUp?.Invoke();
+                _isDown = false;
+            }
+        }
+
         public void OnDrag(PointerEventData eventData)
         {
             var relativeDelta = eventData.delta.x / Screen.width;
-            OnHorizontalSwipe?.Invoke(relativeDelta);
+            if (_isDown)
+                OnHorizontalSwipe?.Invoke(relativeDelta);
         }
 
         public void OnPointerDown(PointerEventData eventData)
         {
+            _isDown = true;
             OnDown?.Invoke();
         }
 
         public void OnPointerUp(PointerEventData eventData)
         {
+            _isDown = false;
             OnUp?.Invoke();
         }
     }
